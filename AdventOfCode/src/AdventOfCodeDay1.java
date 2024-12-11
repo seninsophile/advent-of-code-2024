52b977e55f6fa2311c69509d11ff13691fbeaf5a
1952153 <1952153@01-18118.pusd.dom>
2024-12-11 13:04:06 -0800

    first made repo push commit

---
+++ AdventOfCode/src/AdventOfCodeDay1.java
@@ -0,0 +1,112 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class AdventOfCodeDay1 {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Create a function that will read Adv1
+		 * Sort through Adv1 to get smallest numbers of each list and sort them from
+		 * smallest to largest.
+		 * Then find distance (2 - 1 = 1) or (4 - 1 = 3)
+		 * Make sure distance is not negative
+		 */
+		
+		File file = new File("Adv1");
+		
+		try {
+			
+			Scanner scan = new Scanner(file);
+			
+			ArrayList<Integer> list1 = new ArrayList<Integer>();
+			ArrayList<Integer> list2 = new ArrayList<Integer>();
+			
+			while(scan.hasNext()) {
+				
+//				System.out.println(scan.next());
+				
+				// Will read in two ints per iteration
+				
+				int first = scan.nextInt();
+				int second = scan.nextInt();
+				
+				// Add to list
+				
+				list1.add(first);
+				list2.add(second);
+				
+				// Sort each element by smallest to largest
+				
+				
+				
+			}
+			
+			// for testing only
+			
+			// #1) perform a sequential search in list1, look for smallest element
+			
+			int sum = 0;
+			while(list1.size() > 0){
+				int smallestL = list1.get(0); // assume index 0 = smallest
+				int smallestLIndex = 0;
+				
+				for(int i = 0; i < list1.size(); i++) { // loop to find smallest number
+					
+					if(list1.get(i)<smallestL) {
+						
+						smallestL = list1.get(i);
+						smallestLIndex = i;
+						
+					}
+					
+				}
+
+				// remove smallest value found
+				list1.remove(smallestLIndex);
+
+				int smallestR = list2.get(0); // assume index 0 = smallest
+				int smallestRIndex = 0;
+				
+				for(int i = 0; i < list2.size(); i++) { // loop to find smallest number
+					
+					if(list2.get(i)<smallestR) {
+						
+						smallestR = list2.get(i);
+						smallestRIndex = i;
+						
+					}
+					
+				}
+
+				// remove smallest value found
+				list2.remove(smallestRIndex);
+				
+				// #2) find absolute value difference between both smallests
+				
+//				System.out.println(Math.abs(smallestR - smallestL));
+
+				sum += Math.abs(smallestR - smallestL);
+				
+			}
+			
+			System.out.println(sum);
+			
+			scan.close();
+			
+		} catch (FileNotFoundException e) { // catch both errors
+
+			e.printStackTrace();
+			
+		} catch (NoSuchElementException e) {
+			
+//			e.printStackTrace();
+			
+		}
+		
+	}
+
+}
---
+++ AdventOfCode/src/AdventOfCodeDay2.java
@@ -0,0 +1,109 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class AdventOfCodeDay2 {
+	
+	public static boolean increasing(String[] list) { // helper method
+		
+		for(int i = 1; i < list.length; i++) {
+			
+			// check if does not meet reqs: will exit
+			
+			int el1 = Integer.valueOf(list[i]);
+			int el0 = Integer.valueOf(list[i-1]);
+			if(el1 <= el0) {
+				
+				return false;
+				
+			} else if((Math.abs(el1 - el0) > 3)) { // diff < 3, diff >= 1
+				
+				return false;
+				
+			}
+			
+		}
+		
+		return true;
+		
+	}
+	
+	public static boolean decreasing(String[] list) { // helper method
+		
+		for(int i = 1; i < list.length; i++) {
+			
+			// check if does not meet reqs: will exit
+			
+			int el1 = Integer.valueOf(list[i]);
+			int el0 = Integer.valueOf(list[i-1]);		
+			if(el0 <= el1) {
+				
+				return false;
+				
+			} else if(Math.abs(el0 - el1) > 3) {
+				
+				return false;
+				
+			}
+			
+		}
+		
+		return true;
+		
+	}
+
+
+	public static void main(String[] args) {
+
+		File file = new File("Adv2");
+		
+		try {
+			
+			int count = 0;
+			
+			Scanner scan = new Scanner(file);
+			
+			ArrayList<Integer> listOfFive = new ArrayList<Integer>();
+			
+			String splitFive = "";
+			
+			while(scan.hasNext()) {
+				
+//				System.out.println(scan.nextLine());
+				
+				String line = scan.nextLine();
+				
+				// Split into String array
+				
+				String[] elements = line.split(" ");
+				
+				System.out.println(Arrays.toString(elements));
+				
+				if(increasing(elements) || decreasing(elements)) {
+					count++;
+				}else {
+					System.out.println(Arrays.toString(elements));
+				}
+				
+			}
+			
+			System.out.println(count);
+			
+//			System.out.println(listOfFive);
+			
+			
+			
+			scan.close();
+			
+		} catch (FileNotFoundException e) {
+			
+		} catch (NoSuchElementException e) {
+			
+//			e.printStackTrace();
+
+		}
+	}
+}
